9a2a57e27796b0f54201cab4b269b4445a5d61eb
github username <theoluyi@gmail.com>
2020-10-22 16:16:56 -0400

    to-do list widget done, --pets :(

--- src/Components/Things/Things.js
+++ src/Components/Things/Things.js
@@ -8,14 +8,55 @@ import "./index.css";
 //   BONUS: The user can remove todo items
 
 export default function Things() {
-  const [thingText, setThingText] = useState("");
+  const [newThingText, setNewThingText] = useState("");
+  const [thingsList, setThingsList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addNewThing(newThingText);
+    setNewThingText("");
+    // console.log(e.target.value);
+  };
+
+  const handleChange = (e) => {
+    setNewThingText(e.target.value);
+    // newThingText = e.target.value;
+    // [e.target.name] = e.target.value;
+  };
+
+  const addNewThing = (newThingText) => {
+    let newThingObject = {
+      id: Math.floor(Math.random() * 1000),
+      text: newThingText,
+    };
+
+    setThingsList([...thingsList, newThingObject]);
+  };
+
+  const mapThingsListToJSX = () => {
+    let allTheThings = thingsList.map((thingPojo) => (
+      <li key={thingPojo.id}>{thingPojo.text}</li>
+    ));
+    return allTheThings;
+  };
+
+  const allTheThings = mapThingsListToJSX();
+
   return (
     <div className="things">
       <h1> Things </h1>
-      <form onSubmit={() => console.log("form submitted")}>
-        <input type="text" value={thingText}></input>
-        <button type="submit" name="submit" value="submit"></button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="thing"
+          onChange={handleChange}
+          value={newThingText}
+        />
+        <button type="submit" name="submit">
+          add a thing
+        </button>
       </form>
+      <ul className="my-things">{allTheThings}</ul>
     </div>
   );
 }
