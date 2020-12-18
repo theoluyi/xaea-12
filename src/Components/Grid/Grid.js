23dfd50f9c17f7f63f69554fce16f71d1b7054da
github username <theoluyi@gmail.com>
2020-12-17 23:06:51 -0500

    adds timer

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./index.css";
 import Form from "../Form/Form"
 import Things from "../Things/Things"
+import Timer from "../Timer/Timer"
 
 const NotebookCard = (props) => {
   return <div id={props.color}>{props.children}</div>;
@@ -13,7 +14,7 @@ function Grid() {
       <NotebookCard color="dark-blue" > X </NotebookCard>
       <NotebookCard color="dark-red" > I am an arbitrary child prop in the dark-red notebook</NotebookCard>
       <NotebookCard color="dark-green" > <Things/> </NotebookCard>
-      <NotebookCard color="blue" > </NotebookCard>
+      <NotebookCard color="blue" > <Timer/></NotebookCard>
       <NotebookCard color="red" > </NotebookCard>
       <NotebookCard color="green" > </NotebookCard>
       <NotebookCard color="light-blue" ><Form/></NotebookCard>
