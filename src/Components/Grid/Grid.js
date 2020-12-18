47bf2b86e3006b134a6462633fe6fc035d29bc0c
github username <theoluyi@gmail.com>
2020-12-17 23:00:27 -0500

    adds {props.children}

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./index.css";
+import Form from "../Form/Form"
 
 const NotebookCard = (props) => {
-  return <div id={props.color}>X</div>;
+  return <div id={props.color}>{props.children}</div>;
 };
 
 function Grid() {
   return (
     <div className="full-page">
-      <NotebookCard color="dark-blue" />
-      <NotebookCard color="dark-red" />
+      <NotebookCard color="dark-blue" > X </NotebookCard>
+      <NotebookCard color="dark-red" > I am an arbitrary child prop in the dark-red notebook</NotebookCard>
       <NotebookCard color="dark-green" />
       <NotebookCard color="blue" />
       <NotebookCard color="red" />
       <NotebookCard color="green" />
-      <NotebookCard color="light-blue" />
-      <NotebookCard color="light-red" />
-      <NotebookCard color="light-green" />
+      <NotebookCard color="light-blue" ><Form/></NotebookCard>
+      <NotebookCard color="light-red" > <Form/></NotebookCard>
+      <NotebookCard color="light-green" ><Form/></NotebookCard>
     </div>
   );
 }
